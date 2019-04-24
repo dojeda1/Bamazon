91db8609bd495b14c243c1376155177364be4598
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-04-24 14:07:53 -0600

    App now takes in input ad changes product quantity accordingly.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
-
+// connection.end();
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -27,6 +26,61 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log(res);
-        connection.end();
+        startUp();
+    })
+}
+
+function startUp() {
+    inquirer.prompt([{
+            type: "input",
+            message: "What is the ID of the product you wish to buy?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "How many would you like to buy?",
+            name: "quantity",
+        },
+    ]).then(function (choice) {
+        var itemId = choice.id;
+        var buyQuantity = choice.quantity;
+
+        purchaseCheck(itemId, buyQuantity)
     })
 };
+
+function purchaseCheck(itemId, buyQuantity) {
+    connection.query(
+        "SELECT stock_quantity FROM products WHERE item_id = ?",
+        [itemId],
+        function (err, res) {
+            if (err) throw err;
+
+            var currentStock = res[0].stock_quantity
+            console.log(currentStock);
+
+            if (currentStock < buyQuantity) {
+                console.log("Not enough stock available to complete order.");
+            } else {
+
+                var newQuantity = currentStock - buyQuantity;
+
+                var query = connection.query(
+                    "UPDATE products SET stock_quantity = ? WHERE item_id = ?", [
+                        newQuantity,
+                        itemId
+                    ],
+                    function (err, res) {
+                        if (err) throw err;
+
+                        console.log(res)
+                        console.log(query)
+
+
+
+                    }
+                );
+            }
+        });
+
+}
