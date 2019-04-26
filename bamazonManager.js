ed3f7e382b035caedcf075fadf0170c51e5b7b79
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-04-25 20:27:54 -0600

    Manager.js starts up with 4 choices and the first displays current database info.

---
+++ bamazonManager.js
@@ -0,0 +1,59 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    inquirer.prompt({
+            type: "list",
+            message: "Which action would you like to perform?",
+            name: "action",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+        },
+
+    ).then(function (choice) {
+        console.log(choice.action)
+        switch (choice.action) {
+            case "View Products for Sale":
+                viewProducts();
+                break;
+
+            case "View Low Inventory":
+                viewLowInv();
+                break;
+
+            case "Add to Inventory":
+                addInv();
+                break;
+
+            case "Add New Product":
+                AddProduct();
+                break;
+        }
+
+    })
+});
+
+function viewProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    })
+};
